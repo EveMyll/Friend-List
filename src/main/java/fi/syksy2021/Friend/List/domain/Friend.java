51125fa1c5d1e782d59ba75a584519248c5ae178
EveMyll <eve.myllyneva@myy.haaga-helia.fi>
2021-09-02 18:11:10 +0300

    Friend List 2

--- src/main/java/fi/syksy2021/Friend/List/domain/Friend.java
+++ src/main/java/fi/syksy2021/Friend/List/domain/Friend.java
@@ -1,27 +1,21 @@
 package fi.syksy2021.Friend.List.domain;
 
 public class Friend {
-private String firstName, lastName;
+private String name;
 
-public String getFirstName() {
-	return firstName;
+public String getName() {
+	return name;
 }
 
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
-}
-
-public String getLastName() {
-	return lastName;
-}
-
-public void setLastName(String lastName) {
-	this.lastName = lastName;
+public void setName(String name) {
+	this.name = name;
 }
 
 @Override
 public String toString() {
-	return "Friend [firstName=" + firstName + ", lastName=" + lastName + "]";
+	return "Friend [name=" + name + "]";
 }
 
+
 }
+
